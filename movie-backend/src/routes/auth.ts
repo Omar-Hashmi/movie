94bd087584f82523faeb526119e2447085d57855
Omar-Hashmi <omarhashmi5k@gmail.com>
2025-07-16 11:40:26 +0500

    Remove movie-backend folder from frontend branch

--- movie-backend/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { signup, login } from '../controller/authController';
-
-const router = Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-
-export default router;
